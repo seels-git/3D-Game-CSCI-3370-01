a5dc4580cc9c30c9731dce723b22b48085e1bdcd
Electrix1113 <161494795+Electrix-AI@users.noreply.github.com>
2026-04-02 11:12:51 -0500

    Jump
    I reworked the player movement and now we are able to jump

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,24 +1,30 @@
 using UnityEngine;
-
+using UnityEngine.InputSystem;
 public class PlayerMovement3D : MonoBehaviour
-{
+{   
+
     public float moveSpeed = 5f;
     private Rigidbody rb; 
+    public float jumpspeed = 15f;
+
 
     void Start()
     {
         rb = GetComponent<Rigidbody>();
-
     }
 
     void Update()
     {
-        float horizontal = Input.GetAxis("Horizontal");
+  float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
 
-        Vector3 movement = new Vector3(horizontal,0f,vertical);
+        Vector3 movement = new Vector3(horizontal,0f,vertical) * moveSpeed;
 
-        rb.linearVelocity = movement * moveSpeed;
-
+        rb.MovePosition(rb.position + movement * Time.fixedDeltaTime);
+if (Input.GetKeyDown(KeyCode.Space))
+        {
+                rb.AddForce(Vector3.up*jumpspeed,ForceMode.Impulse);
+        }
     }
 }
+
