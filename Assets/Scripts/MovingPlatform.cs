43a022d3c3f25526c9fbef72e137d209adba265c
crusty47 <andres.montemayor02@utrgv.edu>
2026-05-11 17:00:29 -0500

    Revert "added moving platform"
    This reverts commit e3720654acf8df7a15b463f773ef7f3c47b43c26.

--- Assets/Scripts/MovingPlatform.cs
+++ Assets/Scripts/MovingPlatform.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 
 using UnityEngine;
 
-public class MovingWall : MonoBehaviour
+public class MovingPlatform : MonoBehaviour
 {
     [SerializeField] GameObject pointA;
     [SerializeField] GameObject pointB;
